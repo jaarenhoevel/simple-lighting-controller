7f34243af206f7139c8ae102968f664c9a5d1330
János Arenhövel <jarenhoevel@copting.de>
2021-10-14 17:12:42 +0200

    Add dmx output

--- simple-lighting-controller.ino
+++ simple-lighting-controller.ino
@@ -1,20 +1,40 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <DmxSimple.h>
 
 #define OUT
 
-#define BEAT_BUTTON_PIN D1
-#define BEAT_LED_PIN D5
+#define BEAT_BUTTON_PIN       D1
+#define BEAT_LED_PIN          D5
 
-#define BEAT_TAPS 10
-#define BEAT_TAP_DURATION 5000
+#define DMX_OUTPUT_PIN        D4
+#define DMX_UNIVERSE_SIZE     64
 
-#define LIGHT_COUNT 4
+#define BEAT_TAPS             10
+#define BEAT_TAP_DURATION     5000
+
+#define LIGHT_COUNT           4
+
+#define LIGHT_CHANNEL_DIMMER  0
+#define LIGHT_CHANNEL_STROBE  7
+
+#define LIGHT_CHANNEL_RED     1
+#define LIGHT_CHANNEL_GREEN   2
+#define LIGHT_CHANNEL_BLUE    3
+#define LIGHT_CHANNEL_WHITE   4
+#define LIGHT_CHANNEL_AMBER   5
+#define LIGHT_CHANNEL_UV      6
+
+#define LIGHT_CHANNEL_SPACING 10
+#define LIGHT_FIRST_CHANNEL   1
 
 uint32_t last_beat = 0;
 uint16_t beat_duration = 500; // 1bps ^= 60bpm
 
 uint32_t last_taps[BEAT_TAPS];
+
+uint8_t brightness = 255;
+uint8_t strobe = 0;
 
 CRGB lights[LIGHT_COUNT];
 CRGB base_color = CRGB::Red;
@@ -30,6 +50,25 @@ void crgb_to_rgbwau(CRGB color, OUT uint8_t* r, uint8_t* g, uint8_t* b, uint8_t*
   *a = min(color.r, color.g); // amber is the mixture of red and green
   *u = min(color.r, color.b); // uv is activated when blue and red are active
 } 
+
+void write_dmx_frame(CRGB* lights) {
+  for (uint8_t i = 0; i < sizeof(lights); i ++) {
+    uint8_t start_channel = (i * LIGHT_CHANNEL_SPACING) + LIGHT_FIRST_CHANNEL;
+
+    uint8_t r, g, b, w, a, u;
+    crgb_to_rgbwau(lights[i], &r, &g, &b, &w, &a, &u);
+
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_DIMMER, brightness);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_STROBE, strobe);
+
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_RED, r);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_GREEN, g);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_BLUE, b);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_WHITE, w);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_AMBER, a);
+    DmxSimple.write(start_channel + LIGHT_CHANNEL_UV, u);
+  }
+}
 
 ICACHE_RAM_ATTR void handle_beat_button() {
   // debounce
@@ -79,6 +118,9 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BEAT_BUTTON_PIN), handle_beat_button, FALLING);
 
   Serial.begin(115200);
+
+  DmxSimple.usePin(DMX_OUTPUT_PIN);
+  DmxSimple.maxChannel(DMX_UNIVERSE_SIZE);
 }
 
 void loop() {
